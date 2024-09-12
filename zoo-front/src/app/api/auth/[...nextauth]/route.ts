e57c46915fb9173930fa62afa7400ca3088c55da
Marco Cortes <marcortes.stiven@gmail.com>
2024-09-11 20:26:41 -0600

    configure base frontend project

---
+++ zoo-front/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,46 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch('/your/endpoint', {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      if (user) return true;
+      return false;
+    },
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+  //secret: process.env.AUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
