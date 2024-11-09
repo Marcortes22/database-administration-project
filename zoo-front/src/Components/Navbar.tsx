fc06199948ceab14740f46cd2246aef6aa86d4e1
TosTar26 <ozsanchez2004@gmail.com>
2024-11-09 01:22:31 -0600

    solve bugs

--- zoo-front/src/Components/Navbar.tsx
+++ zoo-front/src/Components/Navbar.tsx
@@ -45,7 +45,7 @@ export default function AppNavbar() {
     { name: 'Tareas', path: '/dashboard/estadisticas/tareas' },
     { name: 'Calificaciones', path: '/dashboard/estadisticas/calificaciones' },
     { name: 'Alimentos', path: '/dashboard/estadisticas/alimentos' },
-    { name: 'Historial', path: '/dashboard/estadisticas/historial' },
+    { name: 'Movimientos', path: '/dashboard/estadisticas/historial' },
 
   ];
 
